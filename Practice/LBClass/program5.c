c6148ca6db6bb661e493ee5aa4936d7d39148fc4
sohelbpathan <sohelbabulalpathan7@gmail.com>
2026-06-20 23:17:25 +0530

    Add files via upload

---
+++ Practice/LBClass/program5.c
@@ -0,0 +1,30 @@
+/*
+Alogritham
+START
+    Accept fist number as No1
+    Accept second number as No2
+    Perfrom addition of No1 & No2
+    Display the result
+
+STOP
+
+*/
+#include<stdio.h>
+
+
+int main()
+{
+    float i,j,k;
+
+    printf("Enter first number:\n");
+    scanf("%f",&i);
+
+    printf("Enter second number:\n");
+    scanf("%f",&j);
+
+    k=i+j;
+
+    printf("Addition is :^%f",k);
+
+    return 0;
+}
