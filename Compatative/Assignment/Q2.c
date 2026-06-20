88e531001cdae8699012758c6dd402ce290a1f46
sohelbpathan <sohelbabulalpathan7@gmail.com>
2026-06-20 23:12:11 +0530

    Add files via upload

---
+++ Compatative/Assignment/Q2.c
@@ -0,0 +1,27 @@
+//Program to print 5 times "MArvellous"on screen.
+
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}    
+/*
+OUTPUT:Marvellous
+       Marvellous
+       Marvellous
+       Marvellous
+       Marvellous
+
+
+*/
